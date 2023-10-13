6c0d59fa3a814f3c0c4f2e44eafcc4533f8d520e
Theryh <theryhanot44@gmail.com>
2023-10-13 15:43:54 +0200

    Ajoutez des fichiers projet.

---
+++ MegaCasting.DBLib/Models/MegaCastingContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaCasting.DBLib.Models
+{
+    public class MegaCastingContext : DbContext
+    {
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("connectionString");
+        }
+        #endregion
+    }
+}
